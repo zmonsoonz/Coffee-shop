96215193fe15419eef31348c732f63f5fbf28ef5
Danila <mok-danil28@mail.ru>
2024-05-01 19:15:14 +0300

    Made about section in main page

--- src/pages/MainPage.js
+++ src/pages/MainPage.js
@@ -1,24 +1,41 @@
 const MainPage = () => {
     return (
-        <section className="main">
-            <div className="container">
-                <header>
-                    <nav>
-                        <ul>
-                            <li>Coffee house</li>
-                            <li>Our coffee</li>
-                            <li>For yout pleasure</li>
-                        </ul>
-                    </nav>
-                </header>
-                <div className="main__title">
-                    <h1>Everything You Love About Coffee</h1>
-                    <h2 className="main__title__text">We makes every day full of energy and taste</h2>
-                    <h2 className="main__title__text">Want to try our beans?</h2>
-                    <button className="main__title__btn">More</button>
+        <>
+            <section className="main">
+                <div className="container">
+                    <header>
+                        <nav>
+                            <ul>
+                                <li>Coffee house</li>
+                                <li>Our coffee</li>
+                                <li>For yout pleasure</li>
+                            </ul>
+                        </nav>
+                    </header>
+                    <div className="main__title">
+                        <h1>Everything You Love About Coffee</h1>
+                        <h2 className="main__title__text">We makes every day full of energy and taste</h2>
+                        <h2 className="main__title__text">Want to try our beans?</h2>
+                        <button className="main__title__btn">More</button>
+                    </div>
                 </div>
-            </div>
-        </section>
+            </section>
+            <section className="about">
+                <div className="container">
+                    <h2 className="about__title">
+                        About Us
+                    </h2>
+                    <p className="about__par">Extremity sweetness difficult behaviour he of. On disposal of as landlord horrible. 
+                        Afraid at highly months do things on at. Situation recommend objection do intention so questions. 
+                        As greatly removed calling pleased improve an. Last ask him cold feel met spot shy want. 
+                        Children me laughing we prospect answered followed. At it went is song that held help face.<br/><br/>
+                        Now residence dashwoods she excellent you. Shade being under his bed her, Much read on as draw. 
+                        Blessing for ignorant exercise any yourself unpacked. Pleasant horrible but confined day end marriage. 
+                        Eagerness furniture set preserved far recommend. Did even but nor are most gave hope. Secure active 
+                        living depend son repair day ladies now.</p>
+                </div>
+            </section>
+        </>
     )
 }
 
